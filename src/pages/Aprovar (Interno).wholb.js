78230f716f6316fc0a8a3912fac2997ba1bc38e7
Jadyla <jadyla@amoradev.com.br>
2023-07-21 14:28:26 -0300

    Add load on 'Aprovar' page

--- src/pages/Aprovar (Interno).wholb.js
+++ src/pages/Aprovar (Interno).wholb.js
@@ -6,7 +6,11 @@ import {
     utils_fmt_date,
     utils_config_items,
     utils_fmt_money_with_prefix,
+    utils_show_hide_section,
     TRANSACAO_PENDENTE,
+    SECTION_STATE_LOADING,
+    SECTION_STATE_DATA,
+    SECTION_STATE_NO_DATA,
 } from "public/utils";
 
 import {
@@ -40,12 +44,16 @@ async function onclick_ver_detalhes(event) {
 
 async function load_pending_transactions() {
     $w("#repeaterTransacoesPendentes").onItemReady( ($item, itemData, index) => {
+        set_sections(SECTION_STATE_DATA);
         utils_config_items($item, g_transacoes_pendentes, itemData);
     });
 
     let transacoes_pendentes = await be_mod_utils_get_pending_transactions(TRANSACAO_PENDENTE);
 
     $w("#repeaterTransacoesPendentes").data = transacoes_pendentes;
+
+    if (!transacoes_pendentes.length)
+        set_sections(SECTION_STATE_NO_DATA);
 }
 
 async function get_transaction_selected_information(transaction_id) {
@@ -54,8 +62,20 @@ async function get_transaction_selected_information(transaction_id) {
     return transacao_detail;
 }
 
+function set_sections(state) {
+    switch (state){
+        case SECTION_STATE_LOADING:
+            return utils_show_hide_section(["#sectionLoading"], ["#sectionPendingTransactions", "#sectionNoData"]);
+        case SECTION_STATE_DATA:
+            return utils_show_hide_section(["#sectionPendingTransactions"], ["#sectionLoading", "#sectionNoData"]);
+        case SECTION_STATE_NO_DATA:
+            return utils_show_hide_section(["#sectionNoData"], ["#sectionLoading", "#sectionPendingTransactions"]);
+    }
+}
+
 
 $w.onReady(function () {
+    set_sections(SECTION_STATE_LOADING);
     load_pending_transactions();
     setInterval(load_pending_transactions, 5000);
     // Write your JavaScript here
