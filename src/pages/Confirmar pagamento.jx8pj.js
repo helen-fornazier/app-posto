cebfb51745f86ed1f48f9ef3a46159dbcc66982d
Jadyla <jadyla@amoradev.com.br>
2023-08-04 12:45:04 -0300

    Style: standardize global vars on 'Confirmar pagamento' page

--- src/pages/Confirmar pagamento.jx8pj.js
+++ src/pages/Confirmar pagamento.jx8pj.js
@@ -22,24 +22,24 @@ import {
     be_mod_utils_get_posto_pct_cashback,
 } from "backend/be_mod_utils";
 
-let porcentagem_cashback;
+let g_porcentagem_cashback;
 
-let saldo_total;
+let g_saldo_total;
 
-let abastecer_values = JSON.parse(wixStorage.local.getItem('abastecer_values'));
-let valor_abastecimento = parseInt(utils_fmt_strip_non_digits(abastecer_values.valor));
-let tipo_combustivel = abastecer_values.tipo_combustivel;
+let g_abastecer_values = JSON.parse(wixStorage.local.getItem('abastecer_values'));
+let g_valor_abastecimento = parseInt(utils_fmt_strip_non_digits(g_abastecer_values.valor));
+let g_tipo_combustivel = g_abastecer_values.tipo_combustivel;
 
-let bomba_information = JSON.parse(wixStorage.local.getItem('bomba_information'));
-let posto_id = bomba_information.postoId;
-let cod_bomba = bomba_information.codBomba;
+let g_bomba_information = JSON.parse(wixStorage.local.getItem('bomba_information'));
+let g_posto_id = g_bomba_information.postoId;
+let g_cod_bomba = g_bomba_information.codBomba;
 
-let tipo_de_pagamento;
+let g_tipo_de_pagamento;
 
 let g_confirmar_pagamento = [
-    {ui: "#textValorAbastecer", type: "text", raw: utils_fmt_money_with_prefix(valor_abastecimento)},
-    {ui: "#textTipoCombustivel", type: "text", raw: tipo_combustivel},
-    {ui: "#textValorTotalResume", type: "text", raw: utils_fmt_money_with_prefix(valor_abastecimento)},
+    {ui: "#textValorAbastecer", type: "text", raw: utils_fmt_money_with_prefix(g_valor_abastecimento)},
+    {ui: "#textTipoCombustivel", type: "text", raw: g_tipo_combustivel},
+    {ui: "#textValorTotalResume", type: "text", raw: utils_fmt_money_with_prefix(g_valor_abastecimento)},
     {ui: "#boxUsarSaldo", onClick: onclick_usar_saldo},
     {ui: "#boxCashback", onClick: onclick_acumular_cashback},
     {ui: "#buttonConfirmarPagamentoAvancar", onClick: onclick_confirmar_pagamento}
@@ -47,18 +47,18 @@ let g_confirmar_pagamento = [
 
 
 async function set_saldo() {
-    saldo_total = await utils_get_saldo()
-    let saldo = saldo_total;
-    if (saldo > valor_abastecimento)
-        saldo = valor_abastecimento;
+    g_saldo_total = await utils_get_saldo()
+    let saldo = g_saldo_total;
+    if (saldo > g_valor_abastecimento)
+        saldo = g_valor_abastecimento;
     $w("#textUsarSaldoDescontar").text = "Descontar " + utils_fmt_money_with_prefix(saldo);
-    porcentagem_cashback = await be_mod_utils_get_posto_pct_cashback(posto_id);
+    g_porcentagem_cashback = await be_mod_utils_get_posto_pct_cashback(g_posto_id);
     $w("#textCashback").text = "Ganhar " + utils_fmt_money_with_prefix(calculate_caskback_value());
     set_resume_values(true);
 }
 
 function calculate_caskback_value() {
-    let cashback = valor_abastecimento * porcentagem_cashback;
+    let cashback = g_valor_abastecimento * g_porcentagem_cashback;
     return Math.trunc(cashback); // Ignore less than 1 cent
 }
 
@@ -78,8 +78,8 @@ function set_resume_values(is_cashback, value) {
         $w("#boxCashback").style.borderWidth = "4px";
         $w("#textCashbackResume").text = "+ " + utils_fmt_money_with_prefix(calculate_caskback_value());
         $w("#textUsarSaldoResume").text = "- " + utils_fmt_money_with_prefix(0);
-        $w("#textValorAPagar").text = utils_fmt_money_with_prefix(valor_abastecimento);
-        tipo_de_pagamento = "cashback";
+        $w("#textValorAPagar").text = utils_fmt_money_with_prefix(g_valor_abastecimento);
+        g_tipo_de_pagamento = "cashback";
     } else {
         $w("#boxUsarSaldo").style.borderColor = APP_COLORS.main;
         $w("#boxUsarSaldo").style.borderWidth = "4px";
@@ -87,9 +87,9 @@ function set_resume_values(is_cashback, value) {
         $w("#boxCashback").style.borderWidth = "1px";
         $w("#textCashbackResume").text = "+ " + utils_fmt_money_with_prefix(0);
         $w("#textUsarSaldoResume").text = "- " + utils_fmt_money_with_prefix(value);
-        let valor_a_pagar = valor_abastecimento + value;
+        let valor_a_pagar = g_valor_abastecimento + value;
         $w("#textValorAPagar").text = utils_fmt_money_with_prefix(valor_a_pagar);
-        tipo_de_pagamento = "pagamento_saldo";
+        g_tipo_de_pagamento = "pagamento_saldo";
     }
     $w("#buttonConfirmarPagamentoAvancar").enable();
 }
@@ -104,23 +104,23 @@ function set_sections(state) {
 }
 
 async function check_saldo() {
-    let saldo = saldo_total;
-    if (saldo <= valor_abastecimento)
+    let saldo = g_saldo_total;
+    if (saldo <= g_valor_abastecimento)
         return (saldo * -1);
-    return valor_abastecimento * -1;
+    return g_valor_abastecimento * -1;
 }
 
 async function onclick_confirmar_pagamento() {
     let member = await utils_get_member();
     let transacao = {
         clienteId: member._id,
-        postoId: posto_id,
-        tipo: tipo_de_pagamento,
-        tipoCombustivel: tipo_combustivel,
-        valor: valor_abastecimento,
-        valorTipo: tipo_de_pagamento == "cashback" ? calculate_caskback_value() : (await check_saldo()),
+        postoId: g_posto_id,
+        tipo: g_tipo_de_pagamento,
+        tipoCombustivel: g_tipo_combustivel,
+        valor: g_valor_abastecimento,
+        valorTipo: g_tipo_de_pagamento == "cashback" ? calculate_caskback_value() : (await check_saldo()),
         situacao: TRANSACAO_PENDENTE,
-        codBomba: cod_bomba,
+        codBomba: g_cod_bomba,
     };
 
     be_mod_utils_cadastrar_transacao(transacao).then((transacao_id) => {
